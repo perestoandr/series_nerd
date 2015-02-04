46f4c0d2f6b96521b75d108125bbdcd6a51dfdbb
Roman Korolev <tenoclock@yandex.ru>
2015-02-04 20:50:59 +0300

    clear noise script

---
+++ m_hero/db_clear_from_noise.py
@@ -0,0 +1,22 @@
+import re
+from apps.main import db, models
+
+def clear():
+    phrases = models.Phrases.query.all()
+    srt = models.Srt.query.all()
+    words = models.UpperWords.query.all()
+
+    for phrase in phrases:
+        phrase.phrase = re.sub(r'[^A-Za-z0-9\!\?\.\,]', '', phrase.phrase)
+
+    for s in srt:
+        s.set_of_words = re.sub(r'[^A-Za-z0-9\!\?\.\,]', '', s.set_of_words)
+        s.list_of_words = re.sub(r'[^A-Za-z0-9\!\?\.\,]', '', s.list_of_words)
+
+    for word in words:
+        word.word = re.sub(r'[^A-Za-z0-9\!\?\.\,]', '', word.word)
+
+    db.session.commit()
+
+if __name__ == '__main__':
+    clear()
