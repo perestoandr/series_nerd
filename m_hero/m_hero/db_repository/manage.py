0fe9bdfc22d4faea5939ee1f4b95afef47ce06f9
Andrey Perestoronin <per4ik256@gmail.com>
2015-06-17 13:46:58 +0300

    deleted some dirs

--- m_hero/m_hero/db_repository/manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-
-if __name__ == '__main__':
-    main(six='<module 'six' from '/home/tenoclock/projects/mixed_hero/.env/lib/python3.4/site-packages/six.py'>')
